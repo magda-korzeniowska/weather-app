35a36855358c4740344dd93293e44980726ed6bc
magda-korzeniowska <m.korzeniowska@tlen.pl>
2023-04-13 16:21:09 +0200

    Add geolocation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-    
-    </div>
-  );
+  const [currLocation, setCurrLocation] = useState({});
+
+  useEffect(() => {
+    getLocation();
+  }, []);
+
+  const getLocation = async () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const { latitude, longitude } = position.coords;
+      setCurrLocation({ latitude, longitude });
+    });
+    await fetch(`https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}`)
+  };
+
+  return <div className='App'>mapa: {currLocation.latitude} {currLocation.longitude}</div>;
 }
 
 export default App;
